d2c105af7618257dc32e2ad428a5838fac379a0b
FramedEmu548 <63079245+FramedEmu548@users.noreply.github.com>
2020-07-11 09:32:52 -0400

    Update Babylon.js

--- game/Babylon.js
+++ game/Babylon.js
@@ -81,29 +81,7 @@
                 scene.render();    
         });
 
-              //Pointer lock
-var canvas = document.getElementById('renderCanvas');
-canvas.requestPointerLock = canvas.requestPointerLock ||
-                            canvas.mozRequestPointerLock;
-
-document.exitPointerLock = document.exitPointerLock ||
-                           document.mozExitPointerLock;
-
-canvas.onclick = function() {
-  canvas.requestPointerLock();
-};
-document.addEventListener('pointerlockchange', lockChangeAlert, false);
-document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
-function lockChangeAlert() {
-  if (document.pointerLockElement === canvas ||
-      document.mozPointerLockElement === canvas) {
-    console.log('Your pointer is locked');
-  } else {
-    console.log('Your pointer is unlocked');  
-  }
-}
-
-        window.addEventListener("resize", function () {
+             	window.addEventListener("resize", function () {
                 engine.resize();
         });});
 
